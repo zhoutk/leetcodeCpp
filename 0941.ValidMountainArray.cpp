1a67731853345b7dd0a17c5a269e121c0cb753c1
zhoutk <zhoutk@189.cn>
2020-11-03 14:45:52 +0800

    0941.ValidMountainArray-official

--- 0941.ValidMountainArray.cpp
+++ 0941.ValidMountainArray.cpp
@@ -10,31 +10,29 @@
 using namespace std;
 
 bool validMountainArray(vector<int>& A) {
-	int N = A.size();
-	int i = 0;
-
-	// 递增扫描
-	while (i + 1 < N && A[i] < A[i + 1]) {
-		i++;
-	}
-
-	// 最高点不能是数组的第一个位置或最后一个位置
-	if (i == 0 || i == N - 1) {
+	int len = A.size();
+	if (len < 3 || A[0] >= A[1])
 		return false;
+	int flag = 0;
+	for (int i = 1; i < len; i++)
+	{
+		if (A[i] == A[i - 1])
+			return false;
+		else if(flag == 0 && A[i] > A[i - 1] || flag == 1 && A[i] < A[i - 1])
+			continue;
+		else if (flag == 0) {
+			flag++;
+		}
+		else {
+			return false;
+		}
 	}
-
-	// 递减扫描
-	while (i + 1 < N && A[i] > A[i + 1]) {
-		i++;
-	}
-
-	return i == N - 1;
+	return flag == 1;
 }
 
 int main()
 {
 	vector<int> row1;
-	row1.push_back(0);
 	row1.push_back(4);
 	row1.push_back(3);
 	row1.push_back(2);
